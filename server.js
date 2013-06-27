9137c1fa1364a620eebe8fb1426aaac22cd46129
chibzu <dstnpreuss@gmail.com>
2013-06-27 06:24:59 +0000

    rebuilt server

--- server.js
+++ server.js
@@ -12,31 +12,38 @@ var remoteCommands = {
 	"ac_up": function(){shell.exec("irsend send_once ac_remote key_up")},
 	"ac_down": function(){shell.exec("irsend send_once ac_remote key_down")},
 	"ac_fan": function(){shell.exec("irsend send_once ac_remote key_fn")},
-	"ac_mode": function(){shell.exec("irsend send_once ac_remote key_mode")}
+	"ac_mode": function(){shell.exec("irsend send_once ac_remote key_mode")},
+	"on": function(){shell.exec("gpio write 1 1")},
+	"off": function(){shell.exec("gpio write 1 0")}
 };
 
 http.createServer( function(request, response){
 	console.log("Serving request for " + request.url);
-	if (request.url.match("/file/")){
-		
+	if (request.url.match("/files/")){
+		shell.exec("file --mime-type ." + request.url, function(err, stdout, stderr){
+			response.writeHead(200, {'Content-Type': stdout.split(" ")[1]});
+			fs.readFile("."+request.url, function(err, data){response.end(data)});
+		});		
 	}
 	else if (request.url.match("/remote_control")){
 		response.writeHead(200, {'Content-Type': 'text/html'});
-		fs.readFile("./controller.html", function(err, data){
-			response.end(data);
-		});
+		fs.readFile("./controller.html", function(err, data){response.end(data);});
 	}
 	else if (request.url.match("/webcam")){
-		response.writeHead(200, {'Content-Type': 'image/jpeg'});
+		response.writeHead(200, {'Content-Type': 'image/jpeg', 'Cache-Control': 'no-store'});
 		shell.exec("fswebcam -", {encoding: "binary", maxBuffer: 200000*1024}, function(error, stdout, stderr){
 			response.write(stdout, "binary");
 			response.end();
 		});
 	}
-	else if (remoteCommands(request.url.slice(1))){
+	else if (remoteCommands[request.url.slice(1)]){
 		remoteCommands[request.url.slice(1)]();
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 		response.end("success");
+	}
+	else {
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		fs.readFile("files/index.html", function(err, data){response.end(data)});
 	}
 	
 }).listen(1337);
